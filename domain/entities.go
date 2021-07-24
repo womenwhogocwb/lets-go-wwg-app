7042199a14caeb1b4ae22a6cb727474170bf5252
Erika Carvalho <erika.carvalho@protonmail.com>
2021-07-24 17:32:21 -0300

    add game entity

---
+++ domain/entities.go
@@ -0,0 +1,28 @@
+package domain
+
+import "time"
+
+type Move string
+
+const (
+	Pedra   Move = "pedra"
+	Papel   Move = "papel"
+	Tesoura Move = "tesoura"
+)
+
+type Result string
+
+const (
+	Victory Result = "victory"
+	Defeat  Result = "defeat"
+	Draw    Result = "draw"
+)
+
+type Game struct {
+	ID         string
+	PlayerName string
+	PlayerMove string
+	HouseMove  string
+	Result     string
+	CreatedAt  time.Time
+}
